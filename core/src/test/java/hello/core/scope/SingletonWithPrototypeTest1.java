5df4dccce3a71d94a899086c41cef92ad388f179
GWH0 <85302576+GWH0@users.noreply.github.com>
2023-07-11 13:57:18 +0000

    ~ section 9

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,10 +1,14 @@
 package hello.core.scope;
 
+import jakarta.inject.Provider;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
+import hello.core.scope.PrototypeTest.PrototypeBean;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
@@ -21,6 +25,35 @@ public class SingletonWithPrototypeTest1 {
         prototypeBean2.addCount();
         Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
 
+    }
+
+    @Test
+    void singletonClinetUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClinetBean.class, PrototypeBean.class);
+        ClinetBean clinetBean1 = ac.getBean(ClinetBean.class);
+        int count1 = clinetBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClinetBean clinetBean2 = ac.getBean(ClinetBean.class);
+        int count2 = clinetBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+
+
+    }
+
+    @Scope("singleton")
+    static class ClinetBean {
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+        
     }
 
     @Scope("prototype")
